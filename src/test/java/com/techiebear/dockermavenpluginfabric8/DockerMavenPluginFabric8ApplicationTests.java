bfc2634735c02d16afbd759f0b3963531f5adbb4
Techie Bear <techiebear2617@gmail.com>
2021-10-10 07:05:19 +0530

    initial commit

---
+++ src/test/java/com/techiebear/dockermavenpluginfabric8/DockerMavenPluginFabric8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiebear.dockermavenpluginfabric8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMavenPluginFabric8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
